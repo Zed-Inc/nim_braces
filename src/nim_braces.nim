d3461209417739d3be0680937ed14bdde179dd8b
Zed <40908679+Zed-Inc@users.noreply.github.com>
2020-06-21 21:23:11 +1000

    Update nim_braces.nim

--- src/nim_braces.nim
+++ src/nim_braces.nim
@@ -1,9 +1,3 @@
-# This is just an example to get you started. A typical binary package
-# uses this file as the main entry point of the application.
-
-
-import os, strutils
-
 import terminal
 import json
 import strutils
